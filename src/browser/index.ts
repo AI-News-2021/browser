471517b180ed6abd1b05b48049efb689afff9985
Jan Czekala <jan.czekala99@web.de>
2021-06-26 21:40:41 +0200

    fix: headless on wrong NODE_ENV

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -2,13 +2,13 @@
 import puppeteer from 'puppeteer-extra'
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isProd = process.env.NODE_ENV === 'production' ? true : false;
 
 const openBrowser = async (proxy?: string) => {
     puppeteer.use(StealthPlugin())
     const browser = await puppeteer.launch({
         // @ts-ignore
-        headless: !!isDev,
+        headless: !!isProd,
         args: [
             proxy ? `--proxy-server=${proxy}` : ''
         ]
