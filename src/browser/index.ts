0ea742cba905e86d45f920a416b3a3cf2b58f21f
Jan Czekala <jan.czekala99@web.de>
2021-07-28 17:58:43 +0200

    path

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -6,7 +6,7 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 const isProd = process.env.NODE_ENV === 'production' ? true : false;
 
 const openBrowser = async (proxy?: string, noCookies?: boolean) => {
-    const noCookiesPath = resolve(__dirname, '../chrome-extensions/no-cookies/')
+    const noCookiesPath = resolve(__dirname, './chrome-extensions/no-cookies/')
     console.log(`extension path ${noCookiesPath}`)
     puppeteer.use(StealthPlugin())
     const browser = await puppeteer.launch({
