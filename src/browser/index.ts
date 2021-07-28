151ba0e5b6fb6277aeca4a56a70093f6940ceb3f
Jan Czekala <jan.czekala99@web.de>
2021-07-28 17:39:52 +0200

    feat: add no-cookies extension

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,17 +1,20 @@
 
+import { resolve } from 'path';
 import puppeteer from 'puppeteer-extra'
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 
 const isProd = process.env.NODE_ENV === 'production' ? true : false;
 
-const openBrowser = async (proxy?: string) => {
+const openBrowser = async (proxy?: string, noCookies?: boolean) => {
+    const noCookiesPath = resolve(__dirname, '../../chrome-extensions/no-cookies')
     puppeteer.use(StealthPlugin())
     const browser = await puppeteer.launch({
         // @ts-ignore
         headless: !!isProd,
         args: [
             proxy ? `--proxy-server=${proxy}` : '',
-            '--no-sandbox', '--disable-setuid-sandbox'
+            '--no-sandbox', '--disable-setuid-sandbox',
+            noCookies ? `--load-extension=${noCookiesPath}` : ''
         ]
     })
 
